9c4972187036f8fb7d9b96fee6b76bb3e7efceef
alanslim <42917548+alanslim@users.noreply.github.com>
2023-05-31 17:07:52 +0600

    Book js and main js

--- book.js
+++ book.js
@@ -1,36 +1,36 @@
-
-const url = "https://www.dbooks.org/api/";
+const log = console.log;
 
 export class Book {
+   
 
-    log = console.log;
     constructor(data){
-        this.id = data[id];
-        this.title = data[title];
-        this.subtitle = data[subtitle];
-        this.authors = data[authors];
-        this.image = data[image];
+        console.log(data);
+        this.id = data.id;
+        this.title = data.title;
+        this.subtitle = data.subtitle;
+        this.authors = data.authors;
+        this.image = data.image;
+        this.user = undefined;
         this.view = document.createElement('div');
-        this.view.insertAdjacentHTML(afterbegin, `<p>${this.id}</p>, <p>${this.title}</p>, <p>${this.subtitle}</p>, <p>${this.authors}</p>, <img src=${this.image}>`);
-
+        this.view.insertAdjacentHTML("afterbegin", `<p>${this.id}</p>, <p>${this.title}</p>, <p>${this.subtitle}</p>, <p>${this.authors}</p>, <img src=${this.image}> <p>Есть в наличии</p>`);
     }
 
 
     giveBook(user){
 
+        this.user = user;
+        this.view.insertAdjacentHTML("afterbegin", `<p>${this.id}</p> <p>${this.title}</p> <p>${this.subtitle}</p> <p>${this.authors}</p> <img src=${this.image}> <p>Выдано: ${this.user}</p>`);
+
     }
+
+    returnBook(user){
+        this.user = undefined;
+        this.view.insertAdjacentHTML("afterbegin", `<p>${this.id}</p>, <p>${this.title}</p>, <p>${this.subtitle}</p>, <p>${this.authors}</p>, <img src=${this.image}> <p>Есть в наличии</p>`);
+    }
+
+
 
 
 }
 
- async function Getbook(){
-
-    let response = await fetch(url);
-    if(response.ok){
-        let json = await response.json();
-    }
-
-
-    
-
- }
+ 
--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-import * as Book from "/book.js";
-import * as Visitor from "/visitor.js";
+import {Book} from "./book.js";
+//import * as Visitor from "./visitor.js";
+const log = console.log;
+const url = "https://www.dbooks.org/api/recent";
+
+async function getBook(){
+    log("getting book");
+    let response = await fetch(url);
+    log(response);
+    if(response.ok){
+        let json = await response.json();
+        log(json);
+        return json;
+    }
+ }
+
+ let bk = [];
+ let response = await getBook()
+
+ for(let book=0; book<response.total; book++){
+
+    bk.push(new Book(response.books[book]));
+
+ }
+
+
+
+
