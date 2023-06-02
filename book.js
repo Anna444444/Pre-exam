45c4f1513574170f86b8fffb1d9a26f7b26fdc20
l1ng1 <pk9942601@gmail.com>
2023-06-02 11:53:11 +0600

    Добавил красоты,пофиксил ширину книг

--- book.js
+++ book.js
@@ -12,6 +12,7 @@ export class Book {
         this.image = data.image;
         this.user = undefined;
         this.view = document.createElement('div');
+        this.view.classList.add('book');
         this.view.insertAdjacentHTML("afterbegin", `<img src=${this.image}>  <p>${this.id}</p> <p>${this.title}</p> <p>${this.subtitle}</p> <p>${this.authors}</p> <p id="isGiven">Есть в наличии</p>`);
     }
 
